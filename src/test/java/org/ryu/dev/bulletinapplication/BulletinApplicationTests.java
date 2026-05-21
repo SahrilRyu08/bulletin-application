715bb769af1da54420383f5f1a67927082406605
sahril saepuloh <SahrilRyu08@github.com>
2026-05-21 19:24:49 +0700

    init project bulletin

---
+++ src/test/java/org/ryu/dev/bulletinapplication/BulletinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ryu.dev.bulletinapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
